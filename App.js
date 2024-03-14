abb72e923501ee9d2c17996286c20bd80f7acc74
mathews <mathewsulike20@gmail.com>
2024-03-14 16:55:27 +0530

    heading updated

--- App.js
+++ App.js
@@ -10,22 +10,6 @@ const heading = React.createElement(
     },                  
     "Heading 1 from react parcel"
   );
-  const heading2 = React.createElement( 
-    "h2",
-    {
-      id: "title",
-    },
-    "Heading 2"
-  );
-  const container = React.createElement(
-    "div",
-    {
-      id: "container",
-    },
-    [heading, heading2]
-  );
-  console.log(heading);
+
   const root = ReactDOM.createRoot(document.getElementById("root"));
-  //passing a react element inside the root
-  //async defer
-  root.render(container);
+  root.render(heading);
